f62628207716cb5d59c8de90b8a3c96adf1e19ec
Benjamin Sullivan <ben@spiceworks.com>
2016-03-28 09:51:26 -0500

    adds support for json-api

--- config/config.exs
+++ config/config.exs
@@ -19,11 +19,22 @@ config :logger, :console,
   format: "$time $metadata[$level] $message\n",
   metadata: [:request_id]
 
-# Import environment specific config. This must remain at the bottom
-# of this file so it overrides the configuration defined above.
-import_config "#{Mix.env}.exs"
-
 # Configure phoenix generators
 config :phoenix, :generators,
   migration: true,
   binary_id: false
+
+config :ja_serializer,
+  key_format: :underscored
+
+config :phoenix, :format_encoders,
+  "json-api": Poison
+
+config :plug, :mimes, %{
+  "application/vnd.api.v0.1a+json" => ["json-api-v0.1a"],
+  "application/vnd.api+json" => ["json-api"]
+}
+
+# Import environment specific config. This must remain at the bottom
+# of this file so it overrides the configuration defined above.
+import_config "#{Mix.env}.exs"
--- web/router.ex
+++ web/router.ex
@@ -10,7 +10,12 @@ defmodule Todo.Router do
   end
 
   pipeline :api do
-    plug :accepts, ["json"]
+    plug :accepts, ["json-api"]
+    plug :fetch_session
+    plug :fetch_flash
+    plug :protect_from_forgery
+    plug :put_secure_browser_headers
+    plug JaSerializer.Deserializer
   end
 
   scope "/", Todo do
@@ -19,8 +24,9 @@ defmodule Todo.Router do
     get "/", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", Todo do
-  #   pipe_through :api
-  # end
+  scope "/api", Todo do
+    pipe_through :api # Use the default browser stack
+    resources "/lists", ListController, except: [:new, :edit]
+  end
+
 end
